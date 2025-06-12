5f11337f09ecc3da3123a9b7296934843f7a02d4
adrielespiritu <adriel.dev.123@gmail.com>
2025-06-13 00:47:51 +0800

    add fragments in menstrual history

--- app/src/main/java/dev/adriele/adolescare/authentication/adapter/PagerAdapter.kt
+++ app/src/main/java/dev/adriele/adolescare/authentication/adapter/PagerAdapter.kt
@@ -1,14 +1,21 @@
 package dev.adriele.adolescare.authentication.adapter
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class PagerAdapter(fragmentActivity: FragmentActivity, private val fragments: List<Fragment>) : FragmentStateAdapter(fragmentActivity) {
+class PagerAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    private val fragments = mutableListOf<Fragment>()
 
     override fun getItemCount(): Int = fragments.size // Number of pages
 
-    override fun createFragment(position: Int): Fragment {
-        return fragments[position]
+    override fun createFragment(position: Int): Fragment = fragments[position]
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateFragments(newFragments: List<Fragment>) {
+        fragments.clear()
+        fragments.addAll(newFragments)
+        notifyDataSetChanged()
     }
 }
